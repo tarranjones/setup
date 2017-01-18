f0c22e97ad48ff8f743802372bbc8d2aba7f10de
tarran <dotfiles@demo.com>
2017-01-18 17:00:21 +0000

    add composer

---
+++ applications/composer/post-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+composer global require friendsofphp/php-cs-fixer
+# composer global require "dts/ebay-sdk-php"
+# composer global require "facebook/php-sdk-v4"
+# composer global require "google/apiclient:dev-master"
+# composer global require "tedivm/stash"
+composer global require "laravel/installer"
+
+# composer install --global
+
+
+# appendtofile "alias tinker=php artisan tinker"  ~/.bash_aliases
+# # appendtofile "alias artisan=php artisan"  ~/.bash_aliases
+# appendtofile 'export PATH="~/.composer/vendor/bin:$PATH"' ~/.bashrc
+
+# . ~/.bashrc
---
+++ applications/composer/pre-install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# install_application php curl git
