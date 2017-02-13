00a8ee6a0d45049b7668e2c976f56fb52487fff5
Tarran Jones <tarrandavidjones@gmail.com>
2017-02-13 11:49:19 +0000

    add-repo/app-get alias linux

--- linux/install.sh
+++ linux/install.sh
@@ -1,9 +1,8 @@
 
 alias app-get=apt-get
 sudo app-get install software-properties-common && alias add-repo="add-apt-repository -y"
+
 sudo add-repo ppa:webupd8team/unstable
-sudo add-repo ppa:webupd8team/tor-browser
-sudo add-repo ppa:webupd8team/sublime-text-3
 sudo app-get update
-sudo app-get build-dep guake
-# sudo app-get install terminator
+# sudo app-get build-dep guake
+sudo app-get install terminator
--- linux/packages/install.sh
+++ linux/packages/install.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-
-alias app-get=app-get
-sudo app-get install software-properties-common && alias add-repo="add-apt-repository"
-
+# Add Repos 
 sudo add-repo ppa:webupd8team/tor-browser -y
 sudo add-repo ppa:webupd8team/sublime-text-3 -y
 
+#Update 
 sudo app-get update
 
-
+#Install
 sudo app-get install tor-browser -y
 sudo app-get install sublime-text-installer -y
